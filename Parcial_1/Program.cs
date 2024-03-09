0748a767691f49ed4d827806a04daf1a0c9e2df3
Sebastián Castro Saldarriaga <setos02@hotmail.com>
2024-03-08 20:21:10 -0500

    1

---
+++ Parcial_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+
+namespace Parcial_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Un vendedor recibe un sueldo base de $XXXX.Además, recibe una comisión del 10 % sobre el valor total de cada venta que realiza.El vendedor desea saber:
+            //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes ?
+            //¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
+            //¿Cuál fue la venta que le generó la mayor comisión?
+            //¿Cuál fue el promedio de las comisiones que recibió por cada venta? 
+
+        }
+    }
+}
